b4126fa4c0b77f792dd6cb72bb8848f71c0629b6
Srinivasa R <srinivasa.r0503@gmail.com>
2023-04-11 13:59:21 +0530

    added all address modes

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -77,14 +77,135 @@ void CPU::clock()
 
 //addressing modes
 
-uint8_t CPU::IMP()
+uint8_t CPU::IMP() //implied
 {
 	fetched = a;
 	return 0;
 }
 
-uint8_t CPU::IMM()
+uint8_t CPU::IMM() //immediate
 {
 	addr_abs = pc++; //absolute address is the program counter
+	return 0;
+}
+
+uint8_t CPU::ZP0() //zero page
+{
+	addr_abs = read(pc++); //absolute address is the value at the address of the program counter
+	addr_abs &= 0x00FF; //mask the address to 8 bits so it fits in the zero page
+	return 0;
+}
+
+uint8_t CPU::ZPX() //zero page X indexed
+{
+	addr_abs = (read(pc++) + x); //absolute address is the value at the address of the program counter + the value of the X register
+	addr_abs &= 0x00FF; //mask the address to 8 bits so it fits in the zero page
+	return 0;
+}
+
+uint8_t CPU::ZPY() //zero page Y indexed
+{
+	addr_abs = (read(pc++) + y); //absolute address is the value at the address of the program counter + the value of the Y register
+	addr_abs &= 0x00FF; //mask the address to 8 bits so it fits in the zero page
+	return 0;
+}
+
+uint8_t CPU::ABS() //absolute
+{
+	uint16_t low = read(pc++); //low byte of the absolute address is the value at the address of the program counter
+	uint16_t high = read(pc++); //high byte of the absolute address is the value at the address of the program counter + 1
+	addr_abs = (high << 8) | low; //combine the two bytes to form the absolute address
+	return 0;
+}
+
+uint8_t CPU::ABX() //absolute X indexed
+{
+	uint16_t low = read(pc++); //low byte of the absolute address is the value at the address of the program counter
+	uint16_t high = read(pc++); //high byte of the absolute address is the value at the address of the program counter + 1
+	addr_abs = (high << 8) | low; //combine the two bytes to form the absolute address
+	addr_abs = addr_abs + x; //add the value of the X register to the absolute address
+
+	if((addr_abs & 0xFF00) != (high << 8)) //if the high byte of the absolute address has changed (i.e. the address has crossed a page boundary)
+	{
+		return 1; //return 1 to indicate an additional cycle is needed
+	}
+	else
+	{
+		return 0; //return 0 to indicate no additional cycle is needed
+	}
+}
+
+uint8_t CPU::ABY() //absolute Y indexed
+{
+	uint16_t low = read(pc++); //low byte of the absolute address is the value at the address of the program counter
+	uint16_t high = read(pc++); //high byte of the absolute address is the value at the address of the program counter + 1
+	addr_abs = (high << 8) | low; //combine the two bytes to form the absolute address
+	addr_abs = addr_abs + y; //add the value of the Y register to the absolute address
+
+	if((addr_abs & 0xFF00) != (high << 8)) //if the high byte of the absolute address has changed (i.e. the address has crossed a page boundary)
+	{
+		return 1; //return 1 to indicate an additional cycle is needed
+	}
+	else
+	{
+		return 0; //return 0 to indicate no additional cycle is needed
+	}
+}
+
+uint8_t CPU::IND() //indirect
+{
+	uint16_t addr_low = read(pc++); //low byte of the address to jump to is the value at the address of the program counter
+	uint16_t addr_high = read(pc++); //high byte of the address to jump to is the value at the address of the program counter + 1
+	uint16_t addr = (addr_high << 8) | addr_low; //combine the two bytes to form the address to jump to
+	uint16_t low = read(addr); //low byte of the absolute address is the value at the address to jump to
+	//adding 1 to addr may cause a page boundary to be crossed, so we need to check for this
+	//if the page boundary is crossed, the location of the high byte of the absolute address is looked for within the same page with rollover of addr
+	if(addr_low == 0x00FF) //simulate page boundary hardware bug
+	{
+		uint16_t high = read(addr & 0xFF00); //high byte of the absolute address is the value at the address to jump to + 1 but looked for in the same page with rollover
+		addr_abs = (high << 8) | low; //combine the two bytes to form the absolute address
+	}
+	else //normal behaviour
+	{
+		uint16_t high = read(++addr); //high byte of the absolute address is the value at the address to jump to + 1
+		addr_abs = (high << 8) | low; //combine the two bytes to form the absolute address
+	}
+	return 0;
+}
+
+uint8_t CPU::IZX() //indirect X indexed (looked for in zero page)
+{
+	uint16_t addr = read(pc++); //address to jump to is the value at the address of the program counter in the zero page
+	uint16_t low = read((addr + x) & 0x00FF); //low byte of the absolute address is the value at the address to jump to + the value of the X register in the zero page
+	uint16_t high = read((addr + x + 1) & 0x00FF); //high byte of the absolute address is the value at the address to jump to + the value of the X register + 1 in the zero page
+	addr_abs = (high << 8) | low; //combine the two bytes to form the absolute address
+	return 0;
+}
+
+uint8_t CPU::IZY() //indirect Y indexed (looked for in zero page)
+{
+	uint16_t addr = read(pc++); //address to jump to is the value at the address of the program counter in the zero page
+	uint16_t low = read(addr & 0x00FF); //low byte of the absolute address is the value at the address to jump to in the zero page
+	uint16_t high = read((addr + 1) & 0x00FF); //high byte of the absolute address is the value at the address to jump to + 1 in the zero page
+	addr_abs = (high << 8) | low; //combine the two bytes to form the absolute address
+	addr_abs = addr_abs + y; //add the value of the Y register to the absolute address
+
+	if((addr_abs & 0xFF00) != (high << 8)) //if the high byte of the absolute address has changed (i.e. the address has crossed a page boundary)
+	{
+		return 1; //return 1 to indicate an additional cycle is needed
+	}
+	else
+	{
+		return 0; //return 0 to indicate no additional cycle is needed
+	}
+}
+
+uint8_t CPU::REL() //relative
+{
+	addr_rel = read(pc++); //relative address is the value at the address of the program counter
+	if(addr_rel & 0x80) //if the high bit is of the lower byte is set
+	{
+		addr_rel |= 0xFF00; //set the high byte to 1s to form a negative number (two's complement)
+	}
 	return 0;
 }
