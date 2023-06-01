c0f4e7c7f035bcc229415d9c9248993b30203a3b
Srinivasa R <srinivasa.r0503@gmail.com>
2023-06-02 05:07:48 +0530

    added controller support - basic

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <SFML/Graphics/Image.hpp>
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Window/Joystick.hpp>
 
 #include "../include/Bus.h"
 #include "../include/Sound.h"
@@ -59,101 +60,181 @@ int main(int argc, char *argv[])
     nes.controller[0] = 0x00;
     window.setFramerateLimit(60);
 
+    sf::Joystick::update();
+    bool joystick = false;
+    int port = 0;
+    for(int i = 0; i < sf::Joystick::Count; i++)
+    {
+        if(sf::Joystick::isConnected(i))
+        {
+            std::cout << "Joystick connected on port " << i << std::endl;
+            joystick = true;
+            port = i;
+            break;
+        }
+    }
+
     while (window.isOpen())
     {   
         sf::Event event;
         while (window.pollEvent(event))
         {
-            switch(event.type)
+            if(!joystick)
             {
-                case sf::Event::Closed:
-                    NES::Sound::destroy();
-                    //save the prg ram to a file
-                    save_file = std::string(argv[1]).substr(0, std::string(argv[1]).size() - 4) + ".sav";
-                    nes.cartridge->save(save_file);
-                    window.close();
-                    break;
-                case sf::Event::KeyPressed:
-                    switch(event.key.code)
-                    {
-                        case sf::Keyboard::Key::X:
+                switch(event.type)
+                {
+                    case sf::Event::Closed:
+                        NES::Sound::destroy();
+                        //save the prg ram to a file
+                        save_file = std::string(argv[1]).substr(0, std::string(argv[1]).size() - 4) + ".sav";
+                        nes.cartridge->save(save_file);
+                        window.close();
+                        break;
+                    case sf::Event::KeyPressed:
+                        switch(event.key.code)
+                        {
+                            case sf::Keyboard::Key::X:
+                                nes.controller[0] |= 0x80;
+                                break;
+                            case sf::Keyboard::Key::Z:
+                                nes.controller[0] |= 0x40;
+                                break;
+                            case sf::Keyboard::Key::A:
+                                nes.controller[0] |= 0x20;
+                                break;
+                            case sf::Keyboard::Key::S:
+                                nes.controller[0] |= 0x10;
+                                break;
+                            case sf::Keyboard::Key::Up:
+                                nes.controller[0] |= 0x08;
+                                nes.controller[0] &= ~0x04;
+                                break;
+                            case sf::Keyboard::Key::Down:
+                                nes.controller[0] |= 0x04;
+                                nes.controller[0] &= ~0x08;
+                                break;
+                            case sf::Keyboard::Key::Left:
+                                nes.controller[0] |= 0x02;
+                                nes.controller[0] &= ~0x01;
+                                break;
+                            case sf::Keyboard::Key::Right:
+                                nes.controller[0] |= 0x01;
+                                nes.controller[0] &= ~0x02;
+                                break;
+                            case sf::Keyboard::Key::R:
+                                nes.reset();
+                                break;
+                            case sf::Keyboard::Key::P:
+                                //save the prg ram to a file
+                                save_file = std::string(argv[1]).substr(0, std::string(argv[1]).size() - 4) + ".sav";
+                                if(nes.cartridge->save(save_file))
+                                    std::cout << "Saved to " << save_file << std::endl;
+                                else
+                                    std::cout << "Failed to save to " << save_file << std::endl;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case sf::Event::KeyReleased:
+                        switch(event.key.code)
+                        {
+                            case sf::Keyboard::Key::X:
+                                nes.controller[0] &= ~0x80;
+                                break;
+                            case sf::Keyboard::Key::Z:
+                                nes.controller[0] &= ~0x40;
+                                break;
+                            case sf::Keyboard::Key::A:
+                                nes.controller[0] &= ~0x20;
+                                break;
+                            case sf::Keyboard::Key::S:
+                                nes.controller[0] &= ~0x10;
+                                break;
+                            case sf::Keyboard::Key::Up:
+                                nes.controller[0] &= ~0x08;
+                                break;
+                            case sf::Keyboard::Key::Down:
+                                nes.controller[0] &= ~0x04;
+                                break;
+                            case sf::Keyboard::Key::Left:
+                                nes.controller[0] &= ~0x02;
+                                break;
+                            case sf::Keyboard::Key::Right:
+                                nes.controller[0] &= ~0x01;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                switch(event.type)
+                {
+                    case sf::Event::Closed:
+                        NES::Sound::destroy();
+                        //save the prg ram to a file
+                        save_file = std::string(argv[1]).substr(0, std::string(argv[1]).size() - 4) + ".sav";
+                        nes.cartridge->save(save_file);
+                        window.close();
+                        break;
+                    default:
+                        //joystick controls
+                        sf::Joystick::update();
+                        if(sf::Joystick::isButtonPressed(port, 0))
                             nes.controller[0] |= 0x80;
-                            break;
-                        case sf::Keyboard::Key::Z:
+                        else
+                            nes.controller[0] &= ~0x80;
+                        if(sf::Joystick::isButtonPressed(port, 1))
                             nes.controller[0] |= 0x40;
-                            break;
-                        case sf::Keyboard::Key::A:
+                        else
+                            nes.controller[0] &= ~0x40;
+                        if(sf::Joystick::isButtonPressed(port, 6))
                             nes.controller[0] |= 0x20;
-                            break;
-                        case sf::Keyboard::Key::S:
+                        else
+                            nes.controller[0] &= ~0x20;
+                        if(sf::Joystick::isButtonPressed(port, 7))
                             nes.controller[0] |= 0x10;
-                            break;
-                        case sf::Keyboard::Key::Up:
+                        else
+                            nes.controller[0] &= ~0x10;
+                        if(sf::Joystick::getAxisPosition(port, sf::Joystick::Axis::Y) < -50)
+                        {
                             nes.controller[0] |= 0x08;
                             nes.controller[0] &= ~0x04;
-                            break;
-                        case sf::Keyboard::Key::Down:
+                        }
+                        else if(sf::Joystick::getAxisPosition(port, sf::Joystick::Axis::Y) > 50)
+                        {
                             nes.controller[0] |= 0x04;
                             nes.controller[0] &= ~0x08;
-                            break;
-                        case sf::Keyboard::Key::Left:
+                        }
+                        else
+                        {
+                            nes.controller[0] &= ~0x08;
+                            nes.controller[0] &= ~0x04;
+                        }
+                        if(sf::Joystick::getAxisPosition(port, sf::Joystick::Axis::X) < -50)
+                        {
                             nes.controller[0] |= 0x02;
                             nes.controller[0] &= ~0x01;
-                            break;
-                        case sf::Keyboard::Key::Right:
+                        }
+                        else if(sf::Joystick::getAxisPosition(port, sf::Joystick::Axis::X) > 50)
+                        {
                             nes.controller[0] |= 0x01;
                             nes.controller[0] &= ~0x02;
-                            break;
-                        case sf::Keyboard::Key::R:
-                            nes.reset();
-                            break;
-                        case sf::Keyboard::Key::P:
-                            //save the prg ram to a file
-                            save_file = std::string(argv[1]).substr(0, std::string(argv[1]).size() - 4) + ".sav";
-                            if(nes.cartridge->save(save_file))
-                                std::cout << "Saved to " << save_file << std::endl;
-                            else
-                                std::cout << "Failed to save to " << save_file << std::endl;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case sf::Event::KeyReleased:
-                    switch(event.key.code)
-                    {
-                        case sf::Keyboard::Key::X:
-                            nes.controller[0] &= ~0x80;
-                            break;
-                        case sf::Keyboard::Key::Z:
-                            nes.controller[0] &= ~0x40;
-                            break;
-                        case sf::Keyboard::Key::A:
-                            nes.controller[0] &= ~0x20;
-                            break;
-                        case sf::Keyboard::Key::S:
-                            nes.controller[0] &= ~0x10;
-                            break;
-                        case sf::Keyboard::Key::Up:
-                            nes.controller[0] &= ~0x08;
-                            break;
-                        case sf::Keyboard::Key::Down:
-                            nes.controller[0] &= ~0x04;
-                            break;
-                        case sf::Keyboard::Key::Left:
+                        }
+                        else
+                        {
                             nes.controller[0] &= ~0x02;
-                            break;
-                        case sf::Keyboard::Key::Right:
                             nes.controller[0] &= ~0x01;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                default:
-                    break;
+                        }
+                        break;
+                }
             }
-        }
+        } 
         
         updateWithAudio();
 
